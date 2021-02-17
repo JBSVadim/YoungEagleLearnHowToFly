a562b5695839c8ee7b42c02021cf192c4e13fcc9
JBSVadim <jarlaksblack@gmail.com>
2021-02-17 18:11:28 +0200

    CSS fix and add Count Modes

--- src/components/CountControl/Count/index.jsx
+++ src/components/CountControl/Count/index.jsx
@@ -10,44 +10,36 @@ class Counter extends Component {
       isAdding: true,
     }
   }
-    countIncriment = () =>
-    this.setState((state, props) => {
-      const {count} = state;
-      const value = +this.props.step 
-      const newCountState = count + value;
-      return {
-        count: newCountState
-      };
-    })
-
-    countDecriment = () =>
-    this.setState(( state, props) => {
-      const {count} = state;
-      const value = +this.props.step 
-      const newCountState = count - value;
-
+    
+    countResult = () => this.setState((state, props) => {
+      const {count, isAdding} = state;
+      const value = +this.props.step;
+      const newCountState = isAdding ? count + value : count - value
       return {
         count: newCountState
       }
-    });
+    })
+       
    
-    toggleMod = () => {
-     
-    }
+    toggleMod = () => this.setState((state, props)=>{
+     const {isAdding} = state
+     return {isAdding: !isAdding}
+    })
   
   render () {
-    const {count} = this.state;
+    const {count, isAdding} = this.state;
+    console.log({isAdding})
     return (
       <article className={styles.container}>
         <h1>Lets Start to Count with JSX</h1>
+        <h2 className={isAdding ? styles.stateMode2 : styles.stateMode}>{isAdding ? 'Adding' : 'Minus'}</h2>
         <div>
           <div>
             <span className={styles.countPlaceholder}>{count}$</span>
             <span className={styles.countPlaceholder}>This is your money, beach {this.props.step}$</span>
           </div>
           <div className={styles.divFix}>
-           <button onClick={this.countIncriment}>Add</button>
-           <button onClick={this.countDecriment}>Addios</button>
+           <button onClick={this.countResult}>Click</button>
            <button onClick={this.toggleMod}>ChangeMode</button>
           </div>
         </div>
